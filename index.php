f093b85e6050134fd0a634fa69375ec1d0de17b6
sebastien rannou <rannou.sebastien@gmail.com>
2010-06-27 16:37:29 +0200

    added a dispatcher to require the correct page.

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>c0depast3r</title>
+    <link rel="stylesheet" type="text/css" charset="utf-8" media="all" href="style.css">
+  </head>
+  <body>
+    <div id="c0nt3nt">
+      <div id="h34d3r">
+	<h1>c0depast3r</h1>
+      </div>
+      <div id="p4g3">
+	<?php require_once('inc/dispatcher.php') ?>
+      </div>
+    </div>
+  </body>
+</html>
