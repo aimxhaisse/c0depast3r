70ff2b226caf36f04c14560f295920b97c31e236
s. rannou <mxs@buffout.org>
2010-10-25 15:35:35 +0200

    removed the ugly sleep, now using the spool directory of the daemon bot :)

--- inc/config.php
+++ inc/config.php
@@ -9,18 +9,10 @@ define('IRC_SERVER', 'irc.epitech.net');
 define('IRC_CHAN', '#nantes');
 define('IRC_INFO', 'http://pastebin.buffout.org http://pastebin.buffout.org http://pastebin.buffout.org');
 
+define('IRC_SPOOL', '/tmp/ircspool/');
+
 // send a message to irc :)
 function irc_notice($message)
 {
-	$sock = fsockopen(IRC_SERVER, 6667);
-	if ($sock) {
-		fwrite($sock, "PASS NOPASS\n\r");
-		fwrite($sock, "NICK " . IRC_NAME . "\n\r");
-		fwrite($sock, "USER " . IRC_NAME . " " . IRC_INFO . "\n\r");
-		fwrite($sock, "JOIN " . IRC_CHAN . "\n\r");
-		fwrite($sock, "PRIVMSG ". IRC_CHAN ." :$message\r\n");
-		fflush($sock);
-		sleep(1);
-		fclose($sock);
-	}
+	file_put_contents(IRC_SPOOL . md5(rand()), $message);
 }
