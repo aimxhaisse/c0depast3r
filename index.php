6a64a9293f7ac876cf68ba457b0f40612f36060d
s. rannou <mxs@buffout.org>
2010-10-25 18:15:05 +0200

    fixed a bug with path

--- index.php
+++ index.php
@@ -13,20 +13,19 @@ db_install();
 ?>
 <html>
   <head>
-    <base href="__"/>
     <title>c0depast3r</title>
     <link rel="stylesheet" type="text/css" charset="utf-8" media="all" href="style.css">
-    <link href="prettify/prettify.css" type="text/css" rel="stylesheet" />
-    <script type="text/javascript" src="prettify/prettify.js"></script>
+    <link href="/prettify/prettify.css" type="text/css" rel="stylesheet" />
+    <script type="text/javascript" src="/prettify/prettify.js"></script>
   </head>
   <body onload="prettyPrint()">
     <div id="c0nt3nt">
       <div id="h34d3r">
-	<img src="banner.png" alt="banner" />
+	<img src="/banner.png" alt="banner" />
       </div>
       <div id="m3nu">
-       <a href="add"><img src="new.png" alt="add a new entry" /></a>
-       <a href="latest"><img src="latest.png" alt="list recent entries" /></a>
+       <a href="add"><img src="/new.png" alt="add a new entry" /></a>
+       <a href="latest"><img src="/latest.png" alt="list recent entries" /></a>
       </div>
       <div id="p4g3">
 	<?php require_once('inc/dispatcher.php') ?>
