661cece32923d02965c1168bdd7beab44705fa28
s. rannou <mxs@sbrk.org>
2013-07-27 00:22:32 +0200

    Updated the chunk of lisp to paste a buffer from emacs.

--- c0depast3r.el
+++ c0depast3r.el
@@ -1,3 +1,18 @@
+;; install deps
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
+(defun load-deps (lst)
+  "auto-install a dep if not there"
+  (if lst
+      (progn (when (not (require (car lst) nil t))
+               (package-install (car lst)))
+	     (load-deps (cdr lst)))))
+
+(load-deps '(http-post-simple))
+
 ;; paste region to c0depast3r
 (require 'http-post-simple)
 (defun c0depast3r-paste ()
